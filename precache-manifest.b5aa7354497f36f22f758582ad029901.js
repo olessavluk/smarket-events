5c061d276ee2361bc178d33666c8ad64b2c1a3ef
Oles Savluk <olessavluk@gmail.com>
2020-09-12 14:03:27 +0100

    Updates

---
+++ precache-manifest.b5aa7354497f36f22f758582ad029901.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875e99db5176a956580708d554674bfe",
+    "url": "https://olessavluk.github.io/smarket-events/index.html"
+  },
+  {
+    "revision": "9c04e4064a22d682c3cc",
+    "url": "https://olessavluk.github.io/smarket-events/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "a6599d6644e4ba922f70",
+    "url": "https://olessavluk.github.io/smarket-events/static/css/main.983a5241.chunk.css"
+  },
+  {
+    "revision": "9c04e4064a22d682c3cc",
+    "url": "https://olessavluk.github.io/smarket-events/static/js/2.def58186.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://olessavluk.github.io/smarket-events/static/js/2.def58186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6599d6644e4ba922f70",
+    "url": "https://olessavluk.github.io/smarket-events/static/js/main.c8f4184e.chunk.js"
+  },
+  {
+    "revision": "69c79f33f15288187ae9",
+    "url": "https://olessavluk.github.io/smarket-events/static/js/runtime-main.14f8d23f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a594f37f8ebf4b035229e5f1512b770.js"
+  "https://olessavluk.github.io/smarket-events/precache-manifest.b5aa7354497f36f22f758582ad029901.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://olessavluk.github.io/smarket-events/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
