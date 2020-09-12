87dc016f298c67f68515628ecf2f7ab0aafb19e8
Oles Savluk <olessavluk@gmail.com>
2020-09-12 13:58:42 +0100

    chore: build

---
+++ precache-manifest.7a594f37f8ebf4b035229e5f1512b770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825834be61aaad5983ec8536a61784af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c04e4064a22d682c3cc",
+    "url": "/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "b1dd668e0c217b39389c",
+    "url": "/static/css/main.983a5241.chunk.css"
+  },
+  {
+    "revision": "9c04e4064a22d682c3cc",
+    "url": "/static/js/2.def58186.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.def58186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1dd668e0c217b39389c",
+    "url": "/static/js/main.f06b9bcd.chunk.js"
+  },
+  {
+    "revision": "878d5e4dc76a2a6b210d",
+    "url": "/static/js/runtime-main.5436e42b.js"
+  }
+]);
